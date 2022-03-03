e3788a223768f3c239b4dd06ed1fefaaf770cf61
Matias Pedelhez <matiaspedelhezinf@gmail.com>
2022-03-03 16:51:22 -0300

    Added mobile restriction

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import Window from "./components/Window/Window";
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="App" useRef>
+  const [screenWidth, setScreenWidth] = React.useState(0);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleScreenWidth);
+    return () => {
+      window.removeEventListener("resize", handleScreenWidth);
+    };
+  }, []);
+
+  function handleScreenWidth(e) {
+    setScreenWidth(window.innerWidth);
+  }
+
+  return screenWidth > 1000 ? ( //
+    <div className="App">
       <Window />
       <a
         href="https://www.linkedin.com/in/matias-pedelhez-a84b061b4/"
@@ -15,6 +28,8 @@ function App() {
         in/matias-pedelhez-a84b061b4/
       </a>
     </div>
+  ) : (
+    <h3 className="error">This site is intended to be opened on desktop.</h3>
   );
 }
 
